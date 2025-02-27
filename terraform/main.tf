227ad68a3725bb9b863b9021992af10d84c8646a
impejonathan <impe.jonathan@gmail.com>
2025-02-27 09:12:20 +0100

    3e test RG + BDD

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,5 +71,5 @@ resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
   name                = "AllowAzureServices"
   server_id           = azurerm_mssql_server.sql_server.id
   start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
 }
